ffcda76488145c1cce8bd7e6c6d52cab5fbb8d3c
BIll DeWeese <bdeweese@onetrust.com>
2023-07-03 13:02:49 -0400

    removed extra forward slash

--- js/index.js
+++ js/index.js
@@ -9,19 +9,19 @@ function onLoad() {
 }
 
 function load_inventory(){
-    fetch('https://faraimapp.com//boat/inventory.json')
+    fetch('https://faraimapp.com/boat/inventory.json')
     .then(response => inventory = response.json())
     .then(data => inventory = data)
     .then(data => load_boatSections());
 }
 function load_boatSections(){
-    fetch('https://faraimapp.com//boat/boatSections.json')
+    fetch('https://faraimapp.com/boat/boatSections.json')
     .then(response => boatSections = response.json())
     .then(data => boatSections = data)
     .then(data => load_quickReferences())
 }
 function load_quickReferences(){
-    fetch('https://faraimapp.com//boat/quickReferences.json')
+    fetch('https://faraimapp.com/boat/quickReferences.json')
     .then(response => quickReferences = response.json())
     .then(data => quickReferences = data)
     .then(data => dataLoad_onComplete())

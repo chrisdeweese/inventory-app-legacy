18f1120c96b4127faa3b3ea1ddd80ce9eee958d9
Chris <chris@deweese.com>
2023-08-06 16:22:56 -0500

    feat: upload xamarin app

---
+++ inventory-app-xamarin/inventory-app/inventory-app/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using inventory_app.Services;
+using inventory_app.Views;
+
+namespace inventory_app
+{
+    public partial class App : Application
+    {
+
+        public App ()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
---
+++ inventory-app-xamarin/inventory-app/inventory-app/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using inventory_app.Models;
+using inventory_app.ViewModels;
+
+namespace inventory_app.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
